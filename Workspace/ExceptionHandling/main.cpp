7743ca3b325cdeb36726ea73486bef6290d886e3
Madanraj Venkatesan <madanraj.venkatesan@gmail.com>
2020-05-31 20:20:19 -0700

    After Learning Exception Handling

---
+++ Workspace/ExceptionHandling/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+double divideCustomException(int i, int j);
+
+class DivideByZeroException : public exception {
+public:
+    virtual const char* what() const noexcept {
+        return "Custom Exception - Divide by zero.";
+    }
+};
+
+double divideCustomException(int i, int j) {
+    if(j == 0) throw DivideByZeroException();
+    else {
+        double result = static_cast<double> (i) / j;
+        return result;
+    }
+}
+
+double divide(int i, int j);
+
+double divide(int i, int j) {
+    if(j == 0) throw 0;
+    else {
+        double result = static_cast<double> (i) / j;
+        return result;
+    }
+}
+
+int main() {
+    try { divide(12, 0); }
+    catch(int &ex) { cerr << "Divide By Zero Exception" << endl; }
+        
+    try { divideCustomException(12, 0); }
+    catch(DivideByZeroException &ex) { cerr << ex.what() << endl; }
+    
+    cout << endl;
+    return 0;
+}
